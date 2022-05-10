aedb82734b59c1a82f5e4060bd81176170e7c1b5
wlucasw <l.panier@hotmail.fr>
2022-05-10 15:33:57 +0200

    import pokemonprops from pokemon componant

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,11 +1,7 @@
 import React from "react"
 import { Pokemon } from "../../components/Pokemon"
+import { pokemonProps } from "../../components/Pokemon"
 import styles from "./Home.module.css"
-
-interface Pokemon {
-  name: string
-  id: number
-}
 
 const pokemonList = [
   {
@@ -26,7 +22,7 @@ const pokemonList = [
   },
 ]
 
-function filterPokemonsByName(pokemons: Pokemon[], name: string) {
+function filterPokemonsByName(pokemons: pokemonProps[], name: string) {
   return pokemons.filter(pokemon => pokemon.name.toUpperCase().includes(name.toUpperCase()))
 }
 
