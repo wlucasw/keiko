f5940dd5e01c14b3c1789b8820d1c00e15cbcf5d
wlucasw <l.panier@hotmail.fr>
2022-05-10 17:44:02 +0200

    Connexion API

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useEffect } from "react"
 import { Pokemon } from "../../components/Pokemon"
 import { Props as PokemonProps } from "../../components/Pokemon"
 import styles from "./Home.module.css"
@@ -7,12 +7,24 @@ function filterPokemonsByName(pokemons: PokemonProps[], name: string) {
   return pokemons.filter(pokemon => pokemon.name.toUpperCase().includes(name.toUpperCase()))
 }
 
+function fetchPokemons() {
+  return fetch("http://localhost:8000/pokemons", { headers: { accept: "application/json" } }).then(response =>
+    response.json(),
+  )
+}
+
 export const Home = () => {
   const [pokemonFilterValue, setPokemonFilterValue] = React.useState("")
+
+  const [pokemonList, updatePokemonList] = React.useState<PokemonProps[]>([])
 
   const onInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setPokemonFilterValue(event.target.value)
   }
+
+  useEffect(() => {
+    fetchPokemons().then(pokemonData => updatePokemonList(pokemonData))
+  }, [pokemonFilterValue])
 
   return (
     <div className={styles.intro}>
@@ -20,8 +32,8 @@ export const Home = () => {
       <div>Tu vas pouvoir apprendre tout ce qu'il faut sur React et attraper des pokemons !</div>
       <input className={styles.input} onChange={onInputChange} value={pokemonFilterValue} />
       <div>
-        {filterPokemonsByName(pokemonList, pokemonFilterValue).map(({ name, id }) => {
-          return <Pokemon name={name} id={id} key={id} />
+        {filterPokemonsByName(pokemonList, pokemonFilterValue).map(({ name, id, height, weight }) => {
+          return <Pokemon name={name} id={id} height={height} weight={weight} key={id} />
         })}
       </div>
     </div>
