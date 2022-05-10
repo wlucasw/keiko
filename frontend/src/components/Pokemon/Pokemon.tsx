4d08d9ce75e53d3a891e558963ab73a712286a37
wlucasw <l.panier@hotmail.fr>
2022-05-10 16:32:52 +0200

    Connect to api to display pokemons

--- frontend/src/components/Pokemon/Pokemon.tsx
+++ frontend/src/components/Pokemon/Pokemon.tsx
@@ -1,14 +1,18 @@
 export interface pokemonProps {
   name: string
   id: number
+  height: number
+  weight: number
 }
 
-export const Pokemon = ({ name, id }: pokemonProps) => {
+export const Pokemon = ({ name, id, height, weight }: pokemonProps) => {
   return (
     <div>
       <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`} />
       <p>Name : {name}</p>
       <p>Numéro : {id}</p>
+      <p>Taille : {height}</p>
+      <p>Poids : {weight}</p>
     </div>
   )
 }
