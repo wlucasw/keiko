c849a94c5b1001d6cfa79d6ac4e383227b73297f
wlucasw <l.panier@hotmail.fr>
2022-05-10 11:49:35 +0200

    Delete defineUrl function and replace it with template literals

--- frontend/src/components/Pokemon/Pokemon.tsx
+++ frontend/src/components/Pokemon/Pokemon.tsx
@@ -3,10 +3,6 @@ interface Props {
   id: string
 }
 
-function defineUrl(id: string) {
-  return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`
-}
-
 export const Pokemon = ({ name, id }: Props) => {
   console.log(name)
   return (
