fb41e6c87c5d761bb5d6c2d7385bd48ca45d04be
wlucasw <l.panier@hotmail.fr>
2022-05-11 15:13:37 +0200

    Add loading screen

---
+++ frontend/src/components/Loader/Loader.tsx
@@ -0,0 +1,3 @@
+import LoaderLogo from "./loader.svg"
+
+export const Loader = () => <img height={400} src={LoaderLogo} alt="pokemon-go-font" />
--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,7 +1,8 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useRef } from "react"
 import { Pokemon } from "../../components/Pokemon"
 import { Props as PokemonProps } from "../../components/Pokemon"
 import styles from "./Home.module.css"
+import { Loader } from "../../components/Loader"
 
 function filterPokemonsByName(pokemons: PokemonProps[], name: string) {
   return pokemons.filter(pokemon => pokemon.name.toUpperCase().includes(name.toUpperCase()))
@@ -15,10 +16,12 @@ async function fetchPokemons() {
 
 export const Home = () => {
   const [pokemonList, updatePokemonList] = React.useState<PokemonProps[]>([])
+  const isLoading = useRef(true)
 
   useEffect(() => {
     const fetchData = async () => {
       const pokemonData = await fetchPokemons()
+      isLoading.current = false
       updatePokemonList(pokemonData)
     }
     fetchData()
@@ -26,15 +29,23 @@ export const Home = () => {
 
   return (
     <div>
-      <div className={styles.title}>
-        <div>Pokedex</div>
-      </div>
+      {isLoading.current ? (
+        <div className={styles.loader}>
+          <Loader />
+        </div>
+      ) : (
+        <div>
+          <div className={styles.title}>
+            <div>Pokedex</div>
+          </div>
 
-      <div className={styles.cardBoard}>
-        {pokemonList.map(({ name, id, height, weight }) => {
-          return <Pokemon name={name} id={id} height={height} weight={weight} key={id} />
-        })}
-      </div>
+          <div className={styles.cardBoard}>
+            {pokemonList.map(({ name, id, height, weight }) => {
+              return <Pokemon name={name} id={id} height={height} weight={weight} key={id} />
+            })}
+          </div>
+        </div>
+      )}
     </div>
   )
 }
